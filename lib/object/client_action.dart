72996ee28eaae9fb31fdef32152864830287fac8
leowyongfoo <jacksonleow6@gmail.com>
2024-05-02 16:08:25 +0800

    2/05/2024- YF (close test) - add new app icon - remove unused packages

--- lib/object/client_action.dart
+++ lib/object/client_action.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 import 'package:optimy_second_device/main.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../fragment/reconnect_dialog.dart';
