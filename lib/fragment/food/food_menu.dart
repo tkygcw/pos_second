59fd4491e8b0c9d4d57f5e9e7fa300bde353dcd8
optimypos <optimypos@gmail.com>
2024-08-27 12:54:24 +0800

    27/08/2024- Backup

--- lib/fragment/food/food_menu.dart
+++ lib/fragment/food/food_menu.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:optimy_second_device/fragment/food/food_menu_content.dart';
 import 'package:optimy_second_device/fragment/food/food_menu_content_mobile.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../../main.dart';
 import '../../notifier/theme_color.dart';
-import '../../object/product.dart';
-import '../../object/search_delegate.dart';
-import '../../page/progress_bar.dart';
-import '../../translation/AppLocalizations.dart';
 
 class FoodMenu extends StatefulWidget {
 
@@ -24,17 +18,12 @@ class _FoodMenuState extends State<FoodMenu> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Consumer<ThemeColor>(builder: (context, ThemeColor color, child) {
-      if(size.height > 500){
-        return Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: FoodMenuContent(color: color),
-        );
-      } else {
-        return Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: FoodMenuContentMobile(color: color),
-        );
-      }
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: size.height > 500 ?
+        FoodMenuContent(color: color):
+        FoodMenuContentMobile(color: color),
+      );
     });
   }
 
